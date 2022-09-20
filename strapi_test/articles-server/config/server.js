eb53e3634183fb1dc7f8301e4fcefce854a34263
Choyoung <lll0858630@gmail.com>
2022-09-20 15:08:49 +0900

    strapi-parctice

---
+++ strapi_test/articles-server/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c38a32b08087e73aaa17094ff26d7f01'),
+    },
+  },
+});
---
+++ strapi_test/articles-server/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '17482380-23c3-4648-b374-56051fa57b9a'
+};
