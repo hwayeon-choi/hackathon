c208624c9bb529ebb6e529859b9a7b51b331aa4c
YongJun Han <yongjun@YongJuns-MacBook-Pro.local>
2022-09-21 15:38:57 +0900

    defaultProps

--- LearnReactNative/components/Greeting.js
+++ LearnReactNative/components/Greeting.js
@@ -4,9 +4,13 @@ import {View, Text} from 'react-native';
 function Greeting(props) {
   return (
     <View>
-      <Text style={{color : props.color}}>안녕하세요 {props.name}!</Text>
+      <Text>안녕하세요 {props.name}!</Text>
     </View>
-  )
+  );
 }
+
+Greeting.defaultProps = {
+  name: '리액트 네이티브',
+};
 
 export default Greeting;
