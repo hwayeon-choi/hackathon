28107d52d253f48d0502fb89b41b739f3b280389
Choyoung <lll0858630@gmail.com>
2022-09-23 18:04:06 +0900

    mySQL 데이터 가져오기

--- mysql_test/mysql-userdata.js
+++ mysql_test/mysql-userdata.js
@@ -1,14 +1,39 @@
 let mysql = require('mysql'); //mysql 모듈 가져오기
 
-console.log(mysql); //조회됨
+// console.log(mysql); //조회됨
 
 
 let connetion = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'fullstack305',
-  database: 'userdata2' 
+  database: 'userdata' 
 });
 
-console.log(connetion); //조회됨
+// console.log(connetion); //조회됨
 
+connetion.connect(); //디버깅: _connectCalled: true
+
+//테이블에서 전체 데이터 가져오기
+connetion.query('SELECT * FROM userInfo', function(error, results){
+  if(error) {
+    console.log(error);
+  } else {
+    console.log(results);
+  }
+});
+
+
+//테이블에서 특정 데이터 가져오기 
+connetion.query('SELECT user_id, password FROM userInfo', function(error, results){
+  if(error) {
+    console.log(error);
+  } else {
+    console.log(results);
+  }
+});
+
+//fields 객체
+
+connetion.end();
+//객체 확인
---
+++ mysql_test/test.js
@@ -0,0 +1,5 @@
+let i = 15;
+if((i>0 || i<=4)) {
+  i++;
+}
+console.log(i);
