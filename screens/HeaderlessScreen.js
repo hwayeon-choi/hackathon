65b7fe8fa6197e31373ca5b3fddcaeb737226c8b
seunghyun kim <kshyun.dev@gmail.com>
2022-09-27 16:32:12 +0900

    머티리얼 내비게이터 완료

---
+++ screens/HeaderlessScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text, Button} from 'react-native';
+
+function HeaderlessScreen({navigation}) {
+  return (
+    <View>
+      <Text>Header가 없네?</Text>
+      <Button onPress={() => navigation.pop()} title="뒤로가기" />
+    </View>
+  );
+}
+
+export default HeaderlessScreen;
