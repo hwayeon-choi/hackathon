501b867d54930e7ac26329591e5498bf0cc41b7e
Hwayeon Choi <hwayeon.choi11@gmail.com>
2022-09-29 18:08:17 +0900

    mysql connect

---
+++ index.js
@@ -0,0 +1,25 @@
+const express    = require('express');
+const mysql      = require('mysql');
+const dbconfig   = require('./config/database.js');
+const connection = mysql.createConnection(dbconfig);
+
+const app = express();
+
+// configuration =========================
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', (req, res) => {
+  res.send('Root');
+});
+
+app.get('/', (req, res) => {
+  connection.query('SELECT * from table_test_user', (error, rows) => {
+    if (error) throw error;
+    console.log('User info is: ', rows);
+    res.send(rows);
+  });
+});
+
+app.listen(app.get('port'), () => {
+  console.log('Express server listening on port ' + app.get('port'));
+});
