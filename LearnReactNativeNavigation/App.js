cbfaa20d9571db8c02aba86afbf6d6a7b8c97a64
YongJun Han <yongjun@YongJuns-MacBook-Pro.local>
2022-10-13 16:20:38 +0900

    drawer

--- LearnReactNativeNavigation/App.js
+++ LearnReactNativeNavigation/App.js
@@ -1,58 +1,53 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import HomeScreen from "./screens/HomeScreen";
-import DetailScreen from "./screens/DetailScreen";
-import { View, Text, TouchableOpacity } from "react-native";
-import HeaderlessScreen from "./screens/HeaderlessScreen";
+import { View, Text, Button } from "react-native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-const Stack = createNativeStackNavigator();
-// console.dir(Stack);
+const Drawer = createDrawerNavigator();
+
+function HomeScreen({navigation}) {
+  return(
+    <View>
+      <Text>Home</Text>
+      <Button title="Drawer 열기" onPress={()=> navigation.openDrawer()}/>
+      <Button title="Setting 열기" onPress={()=>navigation.navigate('Setting')}/>
+    </View>
+  )
+}
+
+function SettingScreen({navigation}) {
+  return(
+    <View>
+      <Text>Setting</Text>
+      <Button title="뒤로가기" onPress={()=> navigation.goBack()}/>
+    </View>
+  )
+}
 
 function App() {
   return(
-  <NavigationContainer>
-    <Stack.Navigator initialRouteName="Home" 
-    // screenOptions={{headerShown : false}}
-    >  
-      {/* initialRouteName = 기본적으로 보여줄 화면의 이름(초기화면)을 가리킨다. 설정하지 않으면 가장 상단의 화면이 보여진다 */}
-      <Stack.Screen name="Home" component={HomeScreen} options={{title : '홈', 
-      headerStyle : {
-        backgroundColor: '#29b6f6'
-      },
-      headerTintColor : '#ffffff',
-      headerTitleStyle : {
-        fontWeight : "bold",
-        fontSize : 20,
-      }
-      }}/>
-      {/* name = 화면의 이름을 설정하는 Props. 공식문서에서 대문자로 작성할 것을 권장함*/}
-      <Stack.Screen name="Detail" component={DetailScreen} 
-        options={{
-          headerBackVisible:false,
-          headerLeft : ({onPress}) => (
-            <TouchableOpacity onPress={onPress}>
-              <Text>Left</Text>
-            </TouchableOpacity>
-          ),
-          headerTitle : ({children}) => (
-            <View>
-              <Text>{children}</Text>
-            </View>
-          ),
-          headerRight : ()=> (
-            <View>
-              <Text>Right</Text>
-            </View>
-          ),
-        }} 
-      /> 
-      {/* 화살표 함수에서 바로 반환하면 중괄호를 생략할수 있지만 (a,b) => a+b; , 만약 바로 반환하는것이 객체라면 소괄호로 감싸줘야 한다 
-      (a,b)=>({a+b})  */}
-      <Stack.Screen name="Headerless" component={HeaderlessScreen} options={{headerShown : false}} />
-    </Stack.Navigator>
-  </NavigationContainer>
-  ) 
+    <NavigationContainer>
+      <Drawer.Navigator 
+        initialRouteName="Home" 
+        drawerPosition="left" 
+        backBehavior="history" 
+        drawerContent={({navigation}) => (
+          <SafeAreaView>
+            <Text>A Custom Drawer</Text>
+            <Button onPress={()=> navigation.closeDrawer()} title='Drawer 닫기'></Button>
+          </SafeAreaView>
+        )}
+        screenOptions={{
+          // headerShown : "false",
+          drawerActiveBackgroundColor : '#fb8c00',
+          drawerActiveTintColor : 'white',
+        }}>
+        <Drawer.Screen name="Home" component={HomeScreen} options={{title:'홈'}}/>
+        <Drawer.Screen name="Setting" component={SettingScreen} options={{title:'설정'}}/>
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default App;
