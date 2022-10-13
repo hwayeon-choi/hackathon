eab2a37734f1d9dc432be4e7c360aca13fe952e9
YongJun Han <yongjun@YongJuns-MacBook-Pro.local>
2022-10-13 19:07:50 +0900

    topTapBar

--- LearnReactNativeNavigation/screens/MainScreen.js
+++ LearnReactNativeNavigation/screens/MainScreen.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Text, View, Button} from "react-native";
 import Icon from "react-native-vector-icons/MaterialIcons";
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialTopTabNavigator();
+
 
 function HomeScreen({navigation}) {
   return (
@@ -40,47 +41,38 @@ function MessageScreen() {
 function MainScreen() {
   return(
       <Tab.Navigator initialRouteName="Home" screenOptions={{
-        tabBarActiveTintColor : "#fb8c00",
         tabBarShowLabel : false,
       }}>
         <Tab.Screen 
           name="Home" 
           component={HomeScreen} 
           options={{
-            title: "홈",
-            tabBarIcon: ({color, size}) => (
-              <Icon name="home" color={color} size={size} />
-            ),
+            tabBarIcon: ({color}) => 
+              <Icon name="home" color={color} size={24} />,
           }}
         />
         <Tab.Screen 
         name="Search" 
         component={SearchScreen} 
         options={{
-          title:"검색",
-          tabBarIcon : ({color, size}) => (
-            <Icon name="search" color={color} size={size} />
-          ),
+            tabBarIcon : ({color}) =>
+              <Icon name="search" color={color} size={24} />
           }} 
           />
         <Tab.Screen 
         name="Notification" 
         component={NotificationScreen} 
         options={{
-          title:"알림",
-          tabBarIcon : ({color, size}) => (
-            <Icon name="notifications" color={color} size={size} />
-          ),
+            tabBarIcon : ({color}) => 
+            <Icon name="notifications" color={color} size={24} />,
           }}
           />
         <Tab.Screen 
         name="Message" 
         component={MessageScreen} 
         options={{
-          title:"메세지",
-          tabBarIcon : ({color, size}) => (
-            <Icon name="message" color={color} size={size} />
-          ),
+            tabBarIcon : ({color}) =>
+            <Icon name="message" color={color} size={24} />,
           }}
           />
       </Tab.Navigator>
