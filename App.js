ec6d0c2dc4f85f9ab0a21b5c62d78a669136a46c
Seunghyun Kim <kshyun.dev@gmail.com>
2022-10-16 11:59:14 +0900

    empty-project-react-native

--- App.js
+++ App.js
@@ -1,25 +1,9 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import MainScreen from './screens/MainScreen';
-import DetailScreen from './screens/DetailScreen';
-
-const Stack = createNativeStackNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Main"
-          /* 홈 탭의 메인 콘텐츠 */
-          component={MainScreen}
-          options={{headerShown: false}}
-        />
-        {/* MainScreen의 버튼을 누르면 이동하게 되는 상세 화면 */}
-        <Stack.Screen name="Detail" component={DetailScreen} /> 
-      </Stack.Navigator>
-    </NavigationContainer>
+    <>
+    </>
   );
 }
 
