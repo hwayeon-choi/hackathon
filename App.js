0af683d5d75df5e8a040280d375aa6531c770fdd
Seunghyun Kim <kshyun.dev@gmail.com>
2022-10-16 19:33:32 +0900

    empty

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { View, Text} from 'react-native';
 
 function App() {
   return (
-    <>
-    </>
+    <View>
+      <Text>프로젝트 생성 시간을 줄이기 위한 빈 프로젝트. 브랜치를 만들어서 쓰시오!!!</Text>
+    </View>
   );
 }
 
