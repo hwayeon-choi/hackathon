ed47ee0c92507152b763198859b3a9ba7d353257
Seunghyun Kim <kshyun.dev@gmail.com>
2022-10-19 21:27:19 +0900

    top10 모듈. 매번 새로운 결과값을 받을 수 있게 로그파일 이름에 현재 시간을 붙임.

--- src/module-top10.js
+++ src/module-top10.js
@@ -21,29 +21,32 @@ http.createServer(function (req, res) {
     res.writeHead(200, {"content-type": "text/html; charset=utf-8"});
     res.write(data);
   } 
-  /* top 10 */
+  /* top 10 모듈*/
   else if(pathname === '/popular'){
-  let sql1 = `SELECT * FROM place ORDER BY favorite DESC LIMIT 10;`
-  connection.query(sql1, function(err, rows, fields){
-    if(err) console.log(err);
-    /* 파일로 저장 */
-    fs.writeFile('./inquiry-log/popular.json', JSON.stringify(rows), 'utf-8',(error)=> {
-      if(error === true) {
-        console.error('this is error');
-      }
-    })
-  });
-  /* 다시 호출 */
-  fs.readFile('./inquiry-log/popular.json', 'utf8', (err, data) => {
-    if (err) {
-      console.error(err)
-      return
-    }
-    console.log(data)
-    res.writeHead(200, {"content-type": "application/json; charset=utf-8"});
-    res.write(JSON.stringify(data))
-  })
-} res.end();
-}).listen(3020, function(){
+    let sql = `SELECT * FROM place ORDER BY favorite DESC LIMIT 10;`
+    connection.query(sql, function(err, rows, fields){
+      if(err) console.log(err);
+      let timeNow = `./inquiry-log/popular${Math.floor(Date.now()/1000)}.json`
+      /* 파일로 저장 */
+      fs.writeFile(timeNow, JSON.stringify(rows), 'utf-8',(error)=> {
+        if(error === true) {
+          console.error('this is error');
+        } else {
+          return
+        }
+      })
+      /* 다시 호출 */
+      fs.readFile(timeNow, 'utf8', (err, data) => {
+        if (err) {
+          console.error(err)
+          return
+        }
+        console.log(data)
+        res.writeHead(200, {"content-type": "application/json; charset=utf-8"});
+        res.write(JSON.stringify(data))
+      })
+    });
+  } res.end();
+}).listen(3000, function(){
   console.log('서버가 작동되고 있습니다!');
 });
