8d9e66fbaa76be4479e93c472f5d6a88906e8bd7
Choyoung <lll0858630@gmail.com>
2022-10-21 18:26:15 +0900

    작업중

--- test/filter-test/src/component/JsonTest.js
+++ test/filter-test/src/component/JsonTest.js
@@ -14,10 +14,32 @@ import data from "../db/all.json";
 // }
 
 function JsonTest() {
+
+  // const getCateNamedata = function(){
+  //   const set = [];
+  //   for(let i=0; i<data.length; i++){
+  //     let getNames = Object.values(data[i]);
+  //     for (let j = 0; j < getNames.length; j++) {
+  //       set.push(data[j]);
+  //       return set;
+  //     }
+  //   }
+  // };
+  const test =()=> {
+    const set = [];
+    {data.map((datas) => (
+      set.push(Object.keys(datas))
+      ))};
+      // console.log(set)
+      const newSet = new Set(set);
+      const newArr = [...newSet];
+      console.log(newArr);
+  }
+
   const onClick = () => {
     //클릭했을 때 이벤트..
     console.log("clicked");
-    // console.log(datas);
+    test(); //객체 전부 뜸 
   };
 
   return (
