5c7f99b76de39ecf9abe78f9bbdc37ad83cc8356
seunghyun kim <kshyun.dev@gmail.com>
2022-10-25 13:31:48 +0900

    useState로 동적으로 텍스트 표현 -> 화면이 아니라 콘솔로 출력되도록 바꿔봄

--- App.js
+++ App.js
@@ -1,28 +1,39 @@
-import React from 'react';
-import { View, Text, Button} from 'react-native';
+import React, { useState, createContext } from 'react';
+import { View, Text, Button } from 'react-native';
 import favoriteModule from './FavoriteModule';
-import LoginStatus from './LoginStatus';
+
+const LoginStatus = () =>{
+  const [isLogin, setLogin] = useState("로그인 하시겠습니까?");
+  return (
+    <>
+      <Button title="로그인"onPress={() => {
+        setLogin(!isLogin),
+        console.log(isLogin ? "로그인 하시겠습니까?" : "로그인 되었습니다.")}
+      } />
+    </>
+  )
+}
 
 function App() {
   return (
       <View>
-        <LoginStatus />
+        <LoginStatus/>
+
         <Button
           title="getLikeOX"
-          onPress={()=> favoriteModule.getLikeOX(1, 3)}>
-        </Button>
+          onPress={()=> favoriteModule.getLikeOX(1, 3)}/>
+
         <Button
           title="postLike"
-          onPress={()=> favoriteModule.postLike("like", 1, 3)}>
-        </Button>
+          onPress={()=> favoriteModule.postLike("like", 1, 3)}/>
+
         <Button
           title="getWishlist"
-          onPress={()=>favoriteModule.getWishlist(1, 3)}>
-        </Button>
+          onPress={()=>favoriteModule.getWishlist(1, 3)}/>
+
         <Button
           title="getPopular"
-          onPress={()=>favoriteModule.getPopular()}>
-        </Button>
+          onPress={()=>favoriteModule.getPopular()}/>
       </View>
   );
 }
