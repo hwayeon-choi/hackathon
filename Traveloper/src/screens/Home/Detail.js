02bbd0b4d47fc961676bdb140c9055a673ebc971
YongJun Han <yongjun@YongJuns-MacBook-Pro.local>
2022-11-04 12:32:36 +0900

    업체 상세페이지

--- Traveloper/src/screens/Home/Detail.js
+++ Traveloper/src/screens/Home/Detail.js
@@ -1,12 +1,44 @@
 import { View, Text, StyleSheet,ImageBackground, ScrollView } from 'react-native'
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { add } from 'react-native-reanimated'
 
 const localJSON = require('../../DB/data.json')
-// const 
+// console.log(localJSON[0])
+const regionData = localJSON.map((data)=>data.region)
+const cityData = localJSON.map((data)=>data.city)
+const addressData = localJSON.map((data)=>data._address)
+const numberData = localJSON.map((data)=>data.call_num)
+const dogData = localJSON.map((data)=>data.dog_type)
+const timeData = localJSON.map((data)=>data.open_time)
+const urlData = localJSON.map((data)=>data.homepage)
+
 
 const Detail = ({route,navigation}) => {
 
+  const [region, setRegion] = useState("")
+  const [city, setCity] = useState("")
+  const [address, setAddress] = useState("")
+  const [number, setNumber] = useState("")
+  const [dog, setDog] = useState("")
+  const [time, setTime] = useState("")
+  const [url, setUrl] = useState("")
+  
   let selectedName = route.params.place
+
+  useEffect(()=> {
+    for(let i=0; i<localJSON.length; i++) {
+      if(selectedName === localJSON[i].company_name) {
+        setRegion(regionData[i])
+        setCity(cityData[i])
+        setAddress(addressData[i])
+        setNumber(numberData[i])
+        setDog(dogData[i])
+        setTime(timeData[i])
+        setUrl(urlData[i])
+      }
+    }
+  })
+
   return (
     <ScrollView>
       {/* 상단 이미지  */}
@@ -19,7 +51,18 @@ const Detail = ({route,navigation}) => {
       {/* 업체명 */}
       <View style={styles.textArea}>
         <Text style={styles.title}>{selectedName}</Text>
-        <Text style={styles.adress}>reigon, city</Text>
+        <Text style={styles.adress}>{region}, {city}</Text>
+      </View>
+      <View style={styles.infoBg}>
+        <Text style={styles.openTime}>영업 시간 : {time}</Text>
+        <Text style={styles.dogtype}>출입 가능한 견종 : {dog}</Text>
+        <Text style={styles.url}>홈페이지 : {url}</Text>
+        <Text style={styles.url}>전화번호 : {number}</Text>
+
+        <View style={{marginTop : 16}}>
+          <Text style={styles.detailAdress}>상세 주소 : {address}</Text>
+          <View style={styles.mapView}></View>
+        </View>
       </View>
     </ScrollView>
     )
@@ -27,8 +70,9 @@ const Detail = ({route,navigation}) => {
 
 const styles = StyleSheet.create({
   textArea : {
-    paddingTop : 24,
-    paddingHorizontal : 16
+    paddingVertical : 16,
+    paddingHorizontal : 16,
+    backgroundColor : "#000"
   },
   topBanner: {
     width: '100%',
@@ -39,32 +83,52 @@ const styles = StyleSheet.create({
     paddingVertical : 24,
   },
   title : {
-    fontSize : 20,
-    color : "#000",
+    fontSize : 18,
+    color : "#fff",
     fontWeight : "900"
   },
   adress : {
+    color : "#fff",
     marginTop : 8,
-    fontSize : 16,
+    fontSize : 14,
     fontWeight : "500"
+  },
+  infoBg : {
+    backgroundColor : "#fff",
+    width : "100%",
+    height : 380,
+    paddingVertical : 24,
+    paddingHorizontal : 16,
+  },
+  openTime :{
+    fontSize : 14,
+    color : "#333",
+    fontWeight : "600",
+    marginBottom : 8
+  },
+  dogtype : {
+    fontSize : 14,
+    color : "#333",
+    fontWeight : "600",
+    marginBottom : 8
+  },
+  url : {
+    fontSize : 14,
+    color : "#333",
+    fontWeight : "600",
+    marginBottom : 8
+  },
+  detailAdress : {
+    fontSize : 14,
+    color : "#333",
+    fontWeight : "600",
+    marginBottom : 8
+  },
+  mapView : {
+    backgroundColor : "#333",
+    width : "100%",
+    height : 200
   }
 })
 
 export default Detail
-
-
-// {
-//   "place_detail_id": 1,
-//   "region": "서울특별시",
-//   "city": "영등포구",
-//   "place_type": "식당",
-//   "company_name": "예가원",
-//   "call_num": "",
-//   "_address": "",
-//   "open_time": "매일 10:00~22:00",
-//   "homepage": "http://www.yegawon.co.kr/",
-//   "parking": "N",
-//   "wifi": "N",
-//   "reservation": "\r",
-//   "dog_type": "소형견"
-// },
