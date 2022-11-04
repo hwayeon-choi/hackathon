320fb34ea687748971b057e8955d45e1d1758cfe
Seunghyun Kim <kshyun.dev@gmail.com>
2022-11-04 14:20:17 +0900

    글자색 바뀜 완료

--- Traveloper/src/components/banner.js
+++ Traveloper/src/components/banner.js
@@ -10,56 +10,106 @@ import dog_surf1 from '../../asset/img/dog_surf1.jpg'
 const Banner = ()=> {
   const [number, setNumber] = useState(1)
   const [image, setImage] = useState(dog_surf2)
+  const [textColor, setTextcolor] = useState("black")
+  
 
   /* 시도 1 */
-  const imageContainer = [dog_surf1, walktogether, restaurant2, photospot,dog_surf2,
-    /* "'../../asset/img/walktogether.jpg'",
-    "'../../asset/img/restaurant2.jpg'",
-    "'../../asset/img/photospot.jpg'",
-    "'../../asset/img/dog_surf2.jpg'",
-    "'../../asset/img/dog_surf1.jpg'", */
-  ]
-
-  /* 시도 2 */
-  // function imageContainer(image){
-  //   return 1 ? "require('../../asset/img/walktogether.jpg')"
-  //     : 2 ? "require('../../asset/img/restaurant2.jpg')"
-  //     : 3 ? "require('../../asset/img/photospot.jpg)"
-  //     : 4 ? "require('../../asset/img/dog_surf2.jpg')"
-  //     : "require('../../asset/img/dog_surf1.jpg')"
-  // }
+  const imageContainer = [dog_surf1, walktogether, restaurant2, photospot,dog_surf2]
 
   /* 타이머 */
   setTimeout(()=>{onIncrease()}, 2000);
 
+  /* 기존 이벤트 */
+  // const onIncrease=()=>{
+  //   if(number<=4){ // 1부터 5까지 증가하고 
+  //     setNumber(number+1) // 숫자
+  //     const getImage = imageContainer[number-1]
+  //     setImage(getImage) // 사진
+  //     console.log(number)
+  //   } else { // 다시 1부터 시작하는 카운터
+  //     setNumber(1) // 숫자
+  //     const getImage = imageContainer[number-1]
+  //     setImage(getImage) // 사진
+  //     console.log(number)
+  //   }
+  // }
+
+  /* 글자색 포함 이벤트 */
   const onIncrease=()=>{
-    if(number<=4){ // 1부터 5까지 증가하고 
-      setNumber(number+1) // 숫자
+    if(number===1){
+      setNumber(number+1)
       const getImage = imageContainer[number-1]
-      setImage(getImage) // 사진
-    } else { // 다시 1부터 시작하는 카운터
-      setNumber(1) // 숫자
+      setImage(getImage) 
+      console.log(number)
+      setTextcolor("white")
+
+    } else if(number===2){
+      setNumber(number+1)
       const getImage = imageContainer[number-1]
-      setImage(getImage) // 사진
+      setImage(getImage) 
+      console.log(number)
+      setTextcolor("black")
+
+    } else if(number===3){
+      setNumber(number+1)
+      const getImage = imageContainer[number-1]
+      setImage(getImage) 
+      console.log(number)
+      setTextcolor("white")
+
+    } else if(number===4){
+      setNumber(number+1)
+      const getImage = imageContainer[number-1]
+      setImage(getImage) 
+      console.log(number)
+      setTextcolor("white")
+
+    } else if(number===5){
+      setNumber(1)
+      const getImage = imageContainer[number-1]
+      setImage(getImage) 
+      console.log(number)
+      setTextcolor("black")
     }
   }
+
+  /* 글자 바꿀거 : 검정글자 기준으로 */
+  // 1: 잘보임 => 검정 그대로
+  // 2: 안보임 => 흰색으로
+  // 3: 잘보임 => 검정 그대로
+  // 4: 안보임 => 흰색으로
+  // 5: 안보임 => 흰색으로
+
+  /* too many rerender */
+  // if 번호가 2번 or 4번 or 5번이면 textColor를 흰색으로 바꿔줘
+  // else textColor를 검정색으로 바꿔줘
+  // if(number===2){
+  //   setTextcolor("white")
+  // } else {
+  //   setTextcolor("black")
+  // }
   
   return(
     <>
-      <ImageBackground 
-        source={image}
-        style={styles.topBanner}
-        onPress={onIncrease}
+      <ImageBackground source={image} style={styles.topBanner} onPress={onIncrease}
       >
-
         <View style={styles.BannerTextContainer}>
-          <Text style={styles.topBannerSubtitle}>남해여행</Text>
-          <Text style={styles.topBannerTitle}>댕댕이와 서핑 즐기기</Text>
+          <Text style={[styles.topBannerSubtitle, {color:textColor}]}>
+            남해여행
+          </Text>
+          <Text style={[styles.topBannerTitle, {color:textColor}]}>
+            댕댕이와 서핑 즐기기
+          </Text>
         </View>
+
         <View style={styles.BannerCounterContainer}>
           {/* <Button title="<" color='gray'/> */}
-          <Text style={styles.BannerCounterPage}>{number}</Text>
-          <Text style={styles.BannerCounterTotal}>/ 5    </Text>
+          <Text style={[styles.BannerCounterPage, {color:textColor}]}>
+            {number}
+          </Text>
+          <Text style={[styles.BannerCounterTotal, {color:textColor}]}>
+            / 5
+          </Text>
           {/* <Button title=">" color='gray' onPress={onIncrease}/> */}
         </View>
       </ImageBackground>
@@ -82,28 +132,25 @@ const styles = StyleSheet.create({
     fontWeight : "700",
     marginRight : 20,
     textAlign : 'right',
-    color : "black"
+    // color : {textColor}
   },
   topBannerSubtitle : {
     fontSize : 14,
     fontWeight : "700",
-    color : "black",
+    // color : {textColor},
     marginRight : 20,
     textAlign : 'right'
-  },
-  BannerTextContainer:{
-    
   },
   BannerCounterPage : {
     marginLeft : 20,
     fontSize : 20,
     fontWeight : "700",
-    color : "black",
+    // color : {textColor},
   },
   BannerCounterTotal : {
     fontSize : 10,
     fontWeight : "700",
-    color : "black",
+    // color : {textColor},
   },
   BannerCounterContainer : {
     flexDirection : 'row',
